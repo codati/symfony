214573967cb6da12d342e50ee34f58132df91cac
Vincent Letourneau <vincent@nanoninja.com>
2016-03-07 15:00:12 +0100

    Ajout de l'action hello

--- src/Aston/FrontBundle/Controller/DefaultController.php
+++ src/Aston/FrontBundle/Controller/DefaultController.php
@@ -3,11 +3,21 @@
 namespace Aston\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AstonFrontBundle:Default:index.html.twig');
+    }
+
+    public function helloAction($name)
+    {
+        return $this->render('AstonFrontBundle:Default:hello.html.twig', array(
+            'prenom' => $name,
+        ));
+
+        // return new Response("<h1>Hello $name</h1>");
     }
 }
